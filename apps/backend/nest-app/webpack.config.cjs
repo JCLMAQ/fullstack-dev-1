6a5e8d677a7c684fe07fa4247a885e1777fae2b8
JCM <jcl.maquinay@gmail.com>
2025-11-24 15:29:09 +0100

    test

--- apps/backend/nest-app/webpack.config.cjs
+++ apps/backend/nest-app/webpack.config.cjs
@@ -2,22 +2,6 @@ const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
 const { join, resolve } = require('path');
 
 module.exports = {
-  target: 'node',
-  resolve: {
-    extensions: ['.ts', '.js'],
-    alias: {
-      // Backend libs
-      '@be/posts': resolve(__dirname, '../../../libs/backend/data-access/posts/src/index.ts'),
-      '@be/users': resolve(__dirname, '../../../libs/backend/data-access/users/src/index.ts'),
-      '@be/images': resolve(__dirname, '../../../libs/backend/data-access/images/src/index.ts'),
-      // DB libs
-      '@db/prisma': resolve(__dirname, '../../../libs/db/index.ts'),
-      '@db/prisma-client': resolve(
-        __dirname,
-        '../../../libs/backend/prisma-client/src/index.ts'
-      ),
-    },
-  },
   output: {
     path: join(__dirname, '../../../dist/apps/backend/nest-app'),
     ...(process.env.NODE_ENV !== 'production' && {
